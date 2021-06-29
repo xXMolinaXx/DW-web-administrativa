3f64b83dc0af09045cb332bd2422d3643e9e8aff
Kenny Molina <kjmolina@live.com>
2021-06-29 12:00:30 -0600

    start

---
+++ DW-administracion/src/app/administrador-empresas/administrador-empresas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-administrador-empresas',
+  templateUrl: './administrador-empresas.component.html',
+  styleUrls: ['./administrador-empresas.component.css']
+})
+export class AdministradorEmpresasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ DW-administracion/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AdministradorEmpresasComponent } from './administrador-empresas/administrador-empresas.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdministradorEmpresasComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
